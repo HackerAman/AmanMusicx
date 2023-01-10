91d303b0ed0742fdfb4a8992bc0498c108a75e71
HackerAman <119696118+HackerAman@users.noreply.github.com>
2023-01-10 17:50:57 +0530

    Update inline.py

--- RaiChu/Player/inline.py
+++ RaiChu/Player/inline.py
@@ -28,13 +28,13 @@ async def inline(client: Client, query: InlineQuery):
         InlineQueryResultArticle(
             title="Pause Stream",
             description=f"Pause the current playout on group call.",
-            thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
+            thumb_url="https://telegra.ph/file/557f64e6f46d9f9c2d5f3.png",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
             title="Resume Stream",
             description=f"Resume the ongoing playout on group call.",
-            thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
+            thumb_url="https://telegra.ph/file/557f64e6f46d9f9c2d5f3.png",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
